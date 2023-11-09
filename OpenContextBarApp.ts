123382565460128663b214f204ec66bef69d8f5a
rachana-visavadiya <rachana.visavadiya@rocket.chat>
2023-11-09 12:07:37 +0530

    app upload

---
+++ OpenContextBarApp.ts
@@ -0,0 +1,144 @@
+import {
+    IAppAccessors,
+    IConfigurationExtend,
+    IEnvironmentRead,
+    IHttp,
+    ILogger,
+    IModify,
+    IPersistence,
+    IRead,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
+import { RoomTypeFilter, UIActionButtonContext } from '@rocket.chat/apps-engine/definition/ui';
+import { BlockElementType, ISectionBlock, IUIKitInteractionHandler, IUIKitResponse, UIKitActionButtonInteractionContext, UIKitBlockInteractionContext, UIKitViewSubmitInteractionContext } from '@rocket.chat/apps-engine/definition/uikit';
+import { IUIKitContextualBarViewParam } from '@rocket.chat/apps-engine/definition/uikit/UIKitInteractionResponder';
+
+
+class OpenCtxBarCommand implements ISlashCommand {
+    
+    public command = 'contextualbar';
+    public i18nParamsExample = 'slashcommand_params';
+    public i18nDescription = 'slashcommand_description';
+    public providesPreview = false;
+
+    constructor(private readonly app: App) {}
+
+    public async executor(context: SlashCommandContext, _read: IRead, modify: IModify): Promise<void> {
+        const triggerId = context.getTriggerId() as string; 
+        const user = context.getSender();
+
+        const contextualbarBlocks = createContextualBarBlocks(modify); 
+
+        await modify.getUiController().openContextualBarView(contextualbarBlocks, { triggerId }, user); 
+    }
+}
+
+
+function createContextualBarBlocks(modify: IModify, viewId?: string): IUIKitContextualBarViewParam {
+    const blocks = modify.getCreator().getBlockBuilder();
+
+    const date = new Date().toISOString();
+
+    blocks.addSectionBlock({
+        text: blocks.newMarkdownTextObject(`The current date-time is\n${date}`), 
+        accessory: { 
+            type: BlockElementType.BUTTON,
+            actionId: 'date',
+            text: blocks.newPlainTextObject('Refresh'),
+            value: date,
+        },
+    });
+
+    return { 
+        id: viewId || 'contextualbarId',
+        title: blocks.newPlainTextObject('Contextual Bar'),
+        submit: blocks.newButtonElement({
+            text: blocks.newPlainTextObject('Submit'),
+        }),
+        blocks: blocks.getBlocks(),
+    };
+}
+
+export class OpenContextBarApp extends App implements IUIKitInteractionHandler {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        await configuration.slashCommands.provideSlashCommand(
+            new OpenCtxBarCommand(this),
+        )
+
+        await configuration.ui.registerButton({
+            actionId: 'my-action-id', 
+            labelI18n: 'my-action-name', 
+            context: UIActionButtonContext.MESSAGE_ACTION, 
+
+            when: {
+                roomTypes: [
+                    RoomTypeFilter.PUBLIC_CHANNEL, 
+                    RoomTypeFilter.PRIVATE_CHANNEL, 
+                    RoomTypeFilter.DIRECT,
+                ],
+                hasAllRoles: ['admin', 'moderator'],
+            }
+        });
+    }
+
+    public async executeBlockActionHandler(context: UIKitBlockInteractionContext, _read: IRead, _http: IHttp, _persistence: IPersistence, modify: IModify) {
+        const data = context.getInteractionData();
+
+        const contextualbarBlocks = createContextualBarBlocks(modify, data.container.id);
+
+        await modify.getUiController().updateContextualBarView(contextualbarBlocks, { triggerId: data.triggerId }, data.user);
+
+        return {
+            success: true,
+        };
+    }
+
+  
+    public async executeViewSubmitHandler(context: UIKitViewSubmitInteractionContext): Promise<IUIKitResponse> {
+        const data = context.getInteractionData()
+        const text = (data.view.blocks[0] as ISectionBlock).text.text;
+       
+        console.log(text);
+
+        return {
+            success: true,
+        };
+    }
+
+    public async executeActionButtonHandler(
+        context: UIKitActionButtonInteractionContext,
+        read: IRead,
+        http: IHttp,
+        persistence: IPersistence,
+        modify: IModify
+    ): Promise<IUIKitResponse> {
+        const { 
+            buttonContext, 
+            actionId, 
+            triggerId, 
+            user, 
+            room, 
+            message,
+        } = context.getInteractionData();
+
+        if (actionId === 'my-action-id') {
+            const blockBuilder = modify.getCreator().getBlockBuilder();
+            
+            return context.getInteractionResponder().openModalViewResponse({
+                title: blockBuilder.newPlainTextObject('Interaction received'),
+                blocks: blockBuilder.addSectionBlock({
+                    text: blockBuilder.newPlainTextObject('We received your interaction, thanks!')
+                }).getBlocks(),
+            });
+        }
+
+        return context.getInteractionResponder().successResponse();
+    }
+
+}
